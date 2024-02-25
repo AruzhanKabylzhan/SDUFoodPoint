830954108fa41cefd194eb8f681bcea9493f4290
AruzhanKabylzhan <161205385+AruzhanKabylzhan@users.noreply.github.com>
2024-02-25 20:58:28 +0600

    Add files via upload

---
+++ mysite/mysite/urls.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('canteen.urls')),
+    path('', include('django.contrib.auth.urls')),
+    path('', include('account.urls')),
+]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+
+# Configure Admin Titles
+admin.site.site_header = "SduFoodPoint's Administration Page"
